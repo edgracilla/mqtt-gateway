16e15f1fd5f661122c7f5b960bd11259128db452
Benj Sicam <bsicam@reekoh.com>
2015-11-19 12:44:54 +0800

    Further optimizations
    - Reduce memory footprint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var mosca    = require('mosca'),
+var isEmpty  = require('lodash.isempty'),
 	platform = require('./platform'),
 	devices  = {},
 	server, port, qos;
@@ -30,9 +30,7 @@ platform.on('message', function (message) {
  * When a new device is added, add it to the list of authorized devices.
  */
 platform.on('adddevice', function (device) {
-	var _ = require('lodash');
-
-	if (!_.isEmpty(device) && !_.isEmpty(device._id)) {
+	if (!isEmpty(device) && !isEmpty(device._id)) {
 		devices[device._id] = device;
 		platform.log('Successfully added ' + device._id + ' to the pool of authorized devices.');
 	}
@@ -44,9 +42,7 @@ platform.on('adddevice', function (device) {
  * When a device is removed or deleted, remove it from the list of authorized devices.
  */
 platform.on('removedevice', function (device) {
-	var _ = require('lodash');
-
-	if (!_.isEmpty(device) && !_.isEmpty(device._id)) {
+	if (!isEmpty(device) && !isEmpty(device._id)) {
 		delete devices[device._id];
 		platform.log('Successfully removed ' + device._id + ' from the pool of authorized devices.');
 	}
@@ -80,19 +76,24 @@ platform.on('close', function () {
  * Listen for the ready event.
  */
 platform.once('ready', function (options, registeredDevices) {
-	var _      = require('lodash'),
-		domain = require('domain'),
-		config = require('./config.json');
+	var map     = require('lodash.map'),
+		trim    = require('lodash.trim'),
+		uniq    = require('lodash.uniq'),
+		clone   = require('lodash.clone'),
+		mosca   = require('mosca'),
+		domain  = require('domain'),
+		indexBy = require('lodash.indexby'),
+		config  = require('./config.json');
 
-	if (options.qos === 0 || _.isEmpty(options.qos))
+	if (options.qos === 0 || isEmpty(options.qos))
 		qos = 0;
 	else
 		qos = parseInt(options.qos);
 
-	if (!_.isEmpty(registeredDevices)) {
-		var tmpDevices = _.clone(registeredDevices, true);
+	if (!isEmpty(registeredDevices)) {
+		var tmpDevices = clone(registeredDevices, true);
 
-		devices = _.indexBy(tmpDevices, '_id');
+		devices = indexBy(tmpDevices, '_id');
 	}
 
 	var dataTopic = options.data_topic || config.data_topic.default;
@@ -102,13 +103,13 @@ platform.once('ready', function (options, registeredDevices) {
 	var tmpTopics = [dataTopic, messageTopic, groupMessageTopic];
 	var authorizedTopics = options.authorized_topics;
 
-	if (!_.isEmpty(authorizedTopics))
+	if (!isEmpty(authorizedTopics))
 		authorizedTopics = tmpTopics.concat(authorizedTopics.split(','));
 	else
 		authorizedTopics = tmpTopics;
 
-	authorizedTopics = _.map(authorizedTopics, _.trim);
-	authorizedTopics = _.indexBy(_.uniq(authorizedTopics));
+	authorizedTopics = map(authorizedTopics, trim);
+	authorizedTopics = indexBy(uniq(authorizedTopics));
 
 	port = options.port;
 	server = new mosca.Server({
@@ -198,15 +199,15 @@ platform.once('ready', function (options, registeredDevices) {
 
 	server.on('ready', function () {
 		server.authorizePublish = function (client, topic, payload, callback) {
-			return callback(null, !_.isEmpty(devices[client.id]) || topic === client.id || !_.isEmpty(authorizedTopics[topic]));
+			return callback(null, !isEmpty(devices[client.id]) || topic === client.id || !isEmpty(authorizedTopics[topic]));
 		};
 
 		server.authorizeSubscribe = function (client, topic, callback) {
-			return callback(null, !_.isEmpty(devices[client.id]) || topic === client.id || !_.isEmpty(authorizedTopics[topic]));
+			return callback(null, !isEmpty(devices[client.id]) || topic === client.id || !isEmpty(authorizedTopics[topic]));
 		};
 
 		server.authorizeForward = function (client, packet, callback) {
-			return callback(null, !_.isEmpty(devices[client.id]));
+			return callback(null, !isEmpty(devices[client.id]));
 		};
 
 		platform.log('MQTT Gateway initialized on port ' + port);
